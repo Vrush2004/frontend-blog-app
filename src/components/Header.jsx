accae851a9a31fbe20da10904eace268d7b7d170
Vrush2004 <vrushalichaube@gmail.com>
2024-07-22 09:43:10 +0530

    Frontend blog app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { images } from '../constants';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section>
+      <header>
+        <div>
+            <img src={images.logo} alt='logo'/>
+        </div>
+        <div>
+            <ul>
+              <li><a href='/'>Home</a></li>
+              <li><a href='/'>Articles</a></li>
+              <li><a href='/'>Pages</a></li>
+              <li><a href='/'>Pricing </a></li>
+              <li><a href='/'>FAQ</a></li>
+            </ul>
+            <button>Sign In</button>
+        </div>
+      </header>
+    </section>
   )
 }
 
---
+++ src/constants/images.js
@@ -0,0 +1,7 @@
+import logo from '../assets/logo.avif';
+
+const images = {
+    logo,
+}
+
+export default images;
