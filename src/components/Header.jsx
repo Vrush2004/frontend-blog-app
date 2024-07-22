943a3a57ec04ff7cde47fa063544dd8320750168
Vrush2004 <vrushalichaube@gmail.com>
2024-07-22 14:58:42 +0530

    Frontend blog app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
 import { images } from '../constants';
 
+const NavItemsInfo = [
+  {name: 'Home'},
+  {name: 'Articles'},
+  {name: 'Pages'},
+  {name: 'Pricing'},
+  {name: 'FAQ'},
+];
+
+const NavItem = ({name}) => {
+  return (
+    <li className='relative group'>
+    <a href='/' className='px-4 py-2'>
+    {name}
+    </a>
+    <span className='text-green-900 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>/</span>
+    </li>
+  );
+};
+
 const Header = () => {
+const [navIsVisible, setNavIsVisible] = useState(false);
+
+const navVisibilityHandler = () => {
+  setNavIsVisible((curState) => {
+    return !curState;
+  })
+}
+
   return (
     <section>
-      <header className='container mx-auto px-5 flex justify-between py-4'>
+      <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
         <div>
-            <img src={images.logo} alt='logo' className="w-20 h-15"/>
+            <img src={images.logo} alt='logo' className="w-16 h-15"/>
         </div>
-        <div className='flex gap-x-9'>
-            <ul className='flex gap-x-5'>
-              <li><a href='/'>Home</a></li>
-              <li><a href='/'>Articles</a></li>
-              <li><a href='/'>Pages</a></li>
-              <li><a href='/'>Pricing </a></li>
-              <li><a href='/'>FAQ</a></li>
+
+        <div className='lg:hidden z-50'>
+          {navIsVisible ? (
+            <AiOutlineClose className=" w-6 h-6" onClick= {navVisibilityHandler} />
+          ) : (
+          <AiOutlineMenu className=" w-6 h-6" onClick={navVisibilityHandler}/>
+          )}
+        </div>
+
+        <div className={`${navIsVisible ? "right-0" : "-right-full"} mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}>
+            <ul className='text-white lg:text-dark-hard flex flex-col lg:flex-row gap-x-2 font-semibold'>
+                {NavItemsInfo.map((item) => (
+                  <NavItem key={item.name} name={item.name} />
+                ))}
             </ul>
-            <button>Sign In</button>
+            <button className='border-2 border-green-900 px-6 py-2 rounded-full text-green-900 font-semibold hover:bg-green-900 hover:text-white transition-all duration-300'>Sign In</button>
         </div>
       </header>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#006600',
+        dark:{
+          hard:'#003300',
+          soft:'#004d00',
+        },
+      },
+      fontFamily:{
+        opensans:["'Open Sans'", "sans-serif"],
+        roboto:["'roboto'","sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
