e7ec247c0096b00944046a4563ea9630997c7607
Vrush2004 <vrushalichaube@gmail.com>
2024-07-23 19:33:30 +0530

    Article Section

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {images} from '../constants';
 
-const ArticleCard = () => {
+const ArticleCard = ({className}) => {
   return (
-    <div>
-        
+    <div className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}>
+        <img src={images.Post1Image} alt='title' className='w-full object-cover object-center h-auto'/>
+        <div className='p-5'>
+            <h2 className='font-roboto font-bold text-xl text-dark-soft'>Future of Work</h2>
+            <p className='text-dark-light mt-3 text-sm'>Majority of people will work in jobs that don't exist today.</p>
+            <div className='flex justify-between flex-nowrap items-center mt-6'>
+                <div className='flex items-center gap-x-2'>
+                    
+                </div>
+                <span>02 May</span>  
+            </div>
+        </div>
     </div>
   )
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,11 @@
 import logo from '../assets/logo.png';
-import HeroImage from '../assets/HeroImage.png'
+import HeroImage from '../assets/HeroImage.png';
+import Post1Image from '../assets/Posts/Post1.jpg'
 
 const images = {
     logo,
     HeroImage,
+    Post1Image,
 }
 
 export default images;
