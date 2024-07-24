84027e0145f17852f3a44269bbd486fa60b84dfc
Vrush2004 <vrushalichaube@gmail.com>
2024-07-24 18:12:26 +0530

    CTA section

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,14 @@ import logo from '../assets/logo.png';
 import HeroImage from '../assets/HeroImage.png';
 import Post1Image from '../assets/Posts/Post1.jpg';
 import PostProfileImage from '../assets/Posts/post-profile1.webp';
+import CTAImage from '../assets/CTAImage.png'
 
 const images = {
     logo,
     HeroImage,
     Post1Image,
     PostProfileImage,
+    CTAImage
 }
 
 export default images;
