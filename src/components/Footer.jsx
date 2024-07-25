f87cd767f1a532d25964dbdecfe477492cebff67
Vrush2004 <vrushalichaube@gmail.com>
2024-07-25 12:34:39 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {FaTelegram, FaLinkedin, FaGithub} from 'react-icons/fa';
+import {FaTelegram, FaLinkedin, FaGithub,FaHeart} from 'react-icons/fa';
 import {images} from '../constants';
 
 const Footer = () => {
   return (
     <section className='bg-dark-hard'>
-        <footer className='container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5'>
-          <div className='col-span-5'>
-            <h3 className='text-dark-light font-bold'>Product</h3>
-            <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
+        <footer className='container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10 '>
+          <div className='col-span-5 md:col-span-4 lg:col-span-2'>
+            <h3 className='text-dark-light font-bold md:text-lg'>Product</h3>
+            <ul className='text-[#959EAD] text-sm mt-5 space-y-4 md:text-base'>
               <li>
                 <a href="/">Landingpage</a>
               </li>
@@ -26,9 +26,9 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className='col-span-5'>
-            <h3 className='text-dark-light font-bold'>Services</h3>
-            <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
+          <div className='col-span-5 md:col-span-4 lg:col-span-2'>
+            <h3 className='text-dark-light font-bold md:text-lg'>Services</h3>
+            <ul className='text-[#959EAD] text-sm mt-5 space-y-4 md:text-base'>
               <li>
                 <a href="/">Documentation</a>
               </li>
@@ -46,9 +46,9 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className='col-span-5'>
-            <h3 className='text-dark-light font-bold'>Company</h3>
-            <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
+          <div className='col-span-5 md:col-span-4 md:col-start-5 lg:col-span-2 lg:col-start-auto'>
+            <h3 className='text-dark-light font-bold md:text-lg'>Company</h3>
+            <ul className='text-[#959EAD] text-sm mt-5 space-y-4 md:text-base'>
               <li>
                 <a href="/">About</a>
               </li>
@@ -63,9 +63,9 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className='col-span-5'>
-            <h3 className='text-dark-light font-bold'>More</h3>
-            <ul className='text-[#959EAD] text-sm mt-5 space-y-4'>
+          <div className='col-span-5 md:col-span-4 lg:col-span-2'>
+            <h3 className='text-dark-light font-bold md:text-lg'>More</h3>
+            <ul className='text-[#959EAD] text-sm mt-5 space-y-4 md:text-base'>
               <li>
                 <a href="/">Documentation</a>
               </li>
@@ -78,12 +78,13 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className='col-span-10'>
-            <img src={images.logo} alt='logo' className='w-12 h-auto mx-auto'/>
-            <p className='text-sm text-dark-light text-center mt-4'>
+
+          <div className='col-span-10 md:order-first md:col-span-4 lg:col-span-2'>
+            <img src={images.logo} alt='logo' className='w-12 h-auto mx-auto md:mx-0'/>
+            <p className='text-sm text-dark-light text-center mt-4 md:text-left md:text-base lg:text-sm'>
               Build a modern and creative website with crealand
             </p>
-            <ul className='flex justify-center items-center mt-5 space-x-4 text-gray-300'>
+            <ul className='flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start'>
                 <li>
                   <a href="/">
                     <FaTelegram className='w-6 h-auto'/>
@@ -101,6 +102,15 @@ const Footer = () => {
                 </li>
             </ul>
 
+          </div>
+
+          <div className=' hidden md:flex flex-col items-center space-y-4 md:col-span-12 lg:col-span-10'>
+              <div className='bg-green-900 text-white p-3 rounded-full'>
+                <FaHeart className='w-7 h-auto'/>
+              </div>
+              <p className='font-bold italic text-dark-light'>
+                Copyright © 2024, Crafted with love.
+              </p>
           </div>
         </footer>
     </section>
