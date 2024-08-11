03027f52237aebea98fb94f7a83ba550d21ed874
Vrush2004 <vrushalichaube@gmail.com>
2024-08-11 18:49:12 +0530

    dynamic posts add in home page

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,7 +2,9 @@ import logo from '../assets/logo.png';
 import HeroImage from '../assets/HeroImage.png';
 import Post1Image from '../assets/Posts/Post1.jpg';
 import PostProfileImage from '../assets/Posts/post-profile1.webp';
-import CTAImage from '../assets/CTAImage.png'
+import CTAImage from '../assets/CTAImage.png';
+const samplePostImage = "/images/sample-img.jpg";
+const userImage = "/images/user.png";
 
 const images = {
     logo,
@@ -10,6 +12,8 @@ const images = {
     Post1Image,
     PostProfileImage,
     CTAImage,
+    samplePostImage,
+    userImage
 }
 
 export default images;
