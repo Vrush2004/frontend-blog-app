18d859f5fc6cdd02a6e3be0973549895ebea7c3f
Vrush2004 <vrushalichaube@gmail.com>
2024-09-07 14:17:36 +0530

    admin layout navbar design

--- src/pages/admin/components/header/Header.jsx
+++ src/pages/admin/components/header/Header.jsx
@@ -1,10 +1,43 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { images } from '../../../../constants'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { AiFillDashboard, AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { FaComments } from 'react-icons/fa'
+import { MdDashboard } from 'react-icons/md'
+import NavItem from './NavItem'
+import NavItemCollapse from './NavItemCollapse'
+
+const MENU_ITEMS = [
+    {
+        title: "Dashboard",
+        link:"/admin",
+        icon: <AiFillDashboard className='text-xl'/>,
+        name: "dashboard",
+        type: "link"
+    },
+    {
+        title: "Comments",
+        link:"/admin/comments",
+        icon: <FaComments className='text-xl'/>,
+        name: "comments",
+        type: "link"
+    },
+    {
+        title: "Posts",
+        content: [
+            {title: "New", link: "/admin/posts/new"}, 
+            {title: "Manage", link: "/admin/posts/manage"}
+        ],
+        icon: <MdDashboard className='text-xl'/>,
+        name: "posts",
+        type: "collapse"
+    }
+]
 
 const Header = () => {
 const [isMenuActive, setIsMenuActive] = useState(false)
+const [activeNavName, setActiveNavName] = useState("dashboard")
+
 const toggleMenuHandler = () => {
     setIsMenuActive((prevState) => !prevState)
 }
@@ -27,6 +60,29 @@ const toggleMenuHandler = () => {
             <div className='fixed inset-0'>
                 {/* underlay */}
                 <div className='fixed inset-0 bg-black opacity-50' onClick={toggleMenuHandler}/>
+                {/* sidebar */}
+                <div className='fixed top-0 bottom-0 left-0 z-50 w-3/4 overflow-auto bg-white p-4'>
+                    <Link>
+                        <img src={images.logo} alt='logo' className='w-16'/>
+                    </Link>
+                    <h4 className='mt-10 font-bold text-[#C7C7C7]'>MAIN MENU</h4>
+                    {/* menu items */}
+                    <div className='mt-6 flex flex-col gap-y-[0.563rem]'>
+                        {MENU_ITEMS.map((item) => 
+                            item.type === "link" ? 
+                            (<NavItem 
+                                title = {item.title} 
+                                link = {item.link}
+                                icon = {item.icon}
+                                name = {item.name}
+                                activeNavName={activeNavName}
+                                setActiveNavName={setActiveNavName}
+                            />
+                            ) : (
+                            <NavItemCollapse/>
+                            ))}
+                    </div>
+                </div>
             </div>
         )}
     </header>
---
+++ src/pages/admin/components/header/NavItemCollapse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavItemCollapse = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default NavItemCollapse
