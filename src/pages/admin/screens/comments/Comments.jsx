9c8ef5fc33ae0b5651f1767c90e2e4f5e1aca5a2
Vrush2004 <vrushalichaube@gmail.com>
2024-09-12 16:02:34 +0530

    comments page

--- src/pages/admin/screens/comments/Comments.jsx
+++ src/pages/admin/screens/comments/Comments.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { useDataTable } from '../../../../hooks/useDataTable'
+import { deleteComment, getAllComments } from '../../../../services/index/comments'
+import DataTable from '../../components/DataTable'
+import { comment } from 'postcss'
 
 const Comments = () => {
+  const {
+    userState,
+    currentPage,
+    searchKeyword,
+    data: commentsData,
+    isLoading,
+    isFetching,
+    isLoadingDeleteData,
+    queryClient,
+    searchKeywordHandler,
+    submitSearchKeywordHandler,
+    deleteDataHandler,
+    setCurrentPage,
+} = useDataTable({
+  dataQueryFn: () => getAllComments(userState.userInfo.token, searchKeyword, currentPage),
+  dataQueryKey: "comments",
+  deleteDataMessage: "Comment is deleted",
+  mutateDeleteFn: ({slug,token}) =>{
+    return deleteComment({
+      commentId: slug,
+      token,
+    })
+  }
+})
+
   return (
-    <div>Comments</div>
+    <DataTable 
+        pageTitle="Manage Comments" 
+        dataListName="Comments" 
+        searchInputPlaceHolder="Search Comments..."
+        searchKeywordOnSubmitHandler={submitSearchKeywordHandler}
+        searchKeywordOnChangeHandler={searchKeywordHandler}
+        searchKeyword={searchKeyword}
+        tableHandlerTitleList={["Author", "Comment","In Respond to", "Created At",""]}
+        isFetching={isFetching}
+        isLoading={isLoading}
+        data={commentsData?.data}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        headers={commentsData?.headers}
+    >
+        
+    </DataTable>
   )
 }
 
