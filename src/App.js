03df48c601792e9c41a7c80ffbbd4f2c52d1e04d
Vrush2004 <vrushalichaube@gmail.com>
2024-09-13 11:48:57 +0530

    Users manage page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ManagePost from './pages/admin/screens/posts/ManagePost';
 import EditPost from './pages/admin/screens/posts/EditPost';
 import Categories from './pages/admin/screens/categories/Categories';
 import EditCategories from './pages/admin/screens/categories/EditCategories';
+import Users from './pages/admin/screens/users/Users';
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
             <Route path='categories/manage' element={<Categories/>}/>
             <Route path='posts/manage/edit/:slug' element={<EditPost/>}/>
             <Route path='categories/manage/edit/:slug' element={<EditCategories/>}/>
+            <Route path='users/manage' element={<Users/>}/>
         </Route>
       </Routes>  
       <Toaster/>
